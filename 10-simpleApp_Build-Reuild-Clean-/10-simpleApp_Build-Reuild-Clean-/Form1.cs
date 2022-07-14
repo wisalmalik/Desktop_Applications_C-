66fa61ab81c7b2bc2149a84b2fe3d4b65f303ac5
wisal_malik <72118640+wisalmalik@users.noreply.github.com>
2022-07-14 15:40:57 +0500

    first commit

---
+++ 10-simpleApp_Build-Reuild-Clean-/10-simpleApp_Build-Reuild-Clean-/Form1.cs
@@ -0,0 +1,15 @@
+namespace _10_simpleApp_Build_Reuild_Clean_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "welcome to github";
+        }
+    }
+}
---
+++ 10-simpleApp_Build-Reuild-Clean-/10-simpleApp_Build-Reuild-Clean-/Program.cs
@@ -0,0 +1,17 @@
+namespace _10_simpleApp_Build_Reuild_Clean_
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
