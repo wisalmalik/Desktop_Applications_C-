b4cb76a424bc04dc0d0dd623f05b6584330527db
wisal_malik <72118640+wisalmalik@users.noreply.github.com>
2022-07-21 10:13:24 +0500

    first commit

---
+++ 16-simpleApp-menustrip/16-simpleApp-menustrip/Form1.cs
@@ -0,0 +1,25 @@
+namespace _16_simpleApp_menustrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void quitAppToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
